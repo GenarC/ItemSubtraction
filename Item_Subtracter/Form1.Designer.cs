433de8c6e2fc5152ce4e97d5a30b41acf52c6282
GenarC <cemgenar43@gmail.com>
2018-07-17 09:03:08 +0300

    Initial Commit

---
+++ Item_Subtracter/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace Item_Subtracter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnImportExcel = new System.Windows.Forms.Button();
+            this.dgItems = new System.Windows.Forms.DataGridView();
+            this.dgSelect = new System.Windows.Forms.DataGridViewCheckBoxColumn();
+            this.dgNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgMPN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgRsMPN = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgArticleNo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgPackQuantity = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgUnitMeasure = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgArticleDesc = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgManufacturer = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dgMHCodeLevel1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableLayoutPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgItems)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnImportExcel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.dgItems, 0, 1);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 100F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(1415, 731);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnImportExcel
+            // 
+            this.btnImportExcel.Location = new System.Drawing.Point(8, 8);
+            this.btnImportExcel.Margin = new System.Windows.Forms.Padding(8);
+            this.btnImportExcel.Name = "btnImportExcel";
+            this.btnImportExcel.Size = new System.Drawing.Size(151, 84);
+            this.btnImportExcel.TabIndex = 0;
+            this.btnImportExcel.Text = "ImportExcel";
+            this.btnImportExcel.UseVisualStyleBackColor = true;
+            this.btnImportExcel.Click += new System.EventHandler(this.btnImportExcel_Click);
+            // 
+            // dgItems
+            // 
+            this.dgItems.AllowUserToAddRows = false;
+            this.dgItems.AllowUserToDeleteRows = false;
+            this.dgItems.AllowUserToOrderColumns = true;
+            this.dgItems.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgItems.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dgSelect,
+            this.dgNo,
+            this.dgMPN,
+            this.dgRsMPN,
+            this.dgArticleNo,
+            this.dgPackQuantity,
+            this.dgUnitMeasure,
+            this.dgArticleDesc,
+            this.dgManufacturer,
+            this.dgPrice,
+            this.dgMHCodeLevel1});
+            this.dgItems.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgItems.Location = new System.Drawing.Point(16, 116);
+            this.dgItems.Margin = new System.Windows.Forms.Padding(16);
+            this.dgItems.Name = "dgItems";
+            this.dgItems.RowTemplate.Height = 24;
+            this.dgItems.Size = new System.Drawing.Size(1383, 599);
+            this.dgItems.TabIndex = 1;
+            // 
+            // dgSelect
+            // 
+            this.dgSelect.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgSelect.HeaderText = "X";
+            this.dgSelect.Name = "dgSelect";
+            this.dgSelect.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.dgSelect.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.Automatic;
+            this.dgSelect.Visible = false;
+            this.dgSelect.Width = 46;
+            // 
+            // dgNo
+            // 
+            this.dgNo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            this.dgNo.HeaderText = "No";
+            this.dgNo.Name = "dgNo";
+            this.dgNo.Width = 55;
+            // 
+            // dgMPN
+            // 
+            this.dgMPN.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgMPN.DefaultCellStyle = dataGridViewCellStyle1;
+            this.dgMPN.HeaderText = "MPN";
+            this.dgMPN.Name = "dgMPN";
+            this.dgMPN.ReadOnly = true;
+            this.dgMPN.Width = 67;
+            // 
+            // dgRsMPN
+            // 
+            this.dgRsMPN.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgRsMPN.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dgRsMPN.HeaderText = "RS MPN";
+            this.dgRsMPN.Name = "dgRsMPN";
+            this.dgRsMPN.ReadOnly = true;
+            this.dgRsMPN.Width = 90;
+            // 
+            // dgArticleNo
+            // 
+            this.dgArticleNo.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgArticleNo.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgArticleNo.HeaderText = "Article No";
+            this.dgArticleNo.Name = "dgArticleNo";
+            this.dgArticleNo.ReadOnly = true;
+            this.dgArticleNo.Width = 98;
+            // 
+            // dgPackQuantity
+            // 
+            this.dgPackQuantity.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dgPackQuantity.HeaderText = "Pack Quantity (SSM)";
+            this.dgPackQuantity.Name = "dgPackQuantity";
+            this.dgPackQuantity.ReadOnly = true;
+            this.dgPackQuantity.Width = 154;
+            // 
+            // dgUnitMeasure
+            // 
+            this.dgUnitMeasure.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCellsExceptHeader;
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgUnitMeasure.DefaultCellStyle = dataGridViewCellStyle4;
+            this.dgUnitMeasure.HeaderText = "Unit Measure (UoM)";
+            this.dgUnitMeasure.Name = "dgUnitMeasure";
+            this.dgUnitMeasure.ReadOnly = true;
+            this.dgUnitMeasure.Width = 5;
+            // 
+            // dgArticleDesc
+            // 
+            this.dgArticleDesc.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            this.dgArticleDesc.HeaderText = "Article Desc";
+            this.dgArticleDesc.Name = "dgArticleDesc";
+            this.dgArticleDesc.ReadOnly = true;
+            // 
+            // dgManufacturer
+            // 
+            this.dgManufacturer.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            this.dgManufacturer.HeaderText = "Manufacturer";
+            this.dgManufacturer.Name = "dgManufacturer";
+            this.dgManufacturer.ReadOnly = true;
+            this.dgManufacturer.Width = 121;
+            // 
+            // dgPrice
+            // 
+            dataGridViewCellStyle5.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dgPrice.DefaultCellStyle = dataGridViewCellStyle5;
+            this.dgPrice.HeaderText = "Price";
+            this.dgPrice.Name = "dgPrice";
+            this.dgPrice.ReadOnly = true;
+            // 
+            // dgMHCodeLevel1
+            // 
+            this.dgMHCodeLevel1.HeaderText = "MH Code Level 1";
+            this.dgMHCodeLevel1.Name = "dgMHCodeLevel1";
+            this.dgMHCodeLevel1.ReadOnly = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1415, 731);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.tableLayoutPanel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgItems)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnImportExcel;
+        private System.Windows.Forms.DataGridView dgItems;
+        private System.Windows.Forms.DataGridViewCheckBoxColumn dgSelect;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgMPN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgRsMPN;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgArticleNo;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgPackQuantity;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgUnitMeasure;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgArticleDesc;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgManufacturer;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgPrice;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dgMHCodeLevel1;
+    }
+}
+
---
+++ Item_Subtracter/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Item_Subtracter.DataModel;
+using Microsoft.Office.Interop.Excel;
+
+namespace Item_Subtracter
+{
+    public partial class Form1 : Form
+    {
+        List<IGrouping<string,MPN_List>> mpn_ime = new List<IGrouping<string, MPN_List>>();
+        List<string> MPN_Listesi = new List<string>();
+        IMEEntities db;
+
+        private Random rnd = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+            db = new IMEEntities();
+            
+            GetAllItems();
+        }
+        
+        private void btnImportExcel_Click(object sender, EventArgs e)
+        {
+            dgItems.Rows.Clear();
+            List<string> Ham_MPN_Listesi = new List<string>();
+            List<string> Donusturulmus_MPN_Listesi = new List<string>();
+            List<MPN> result = new List<MPN>();
+
+
+            if (ChooseFile(Ham_MPN_Listesi))
+            {
+                foreach (string mpn in Ham_MPN_Listesi)
+                {
+                    Donusturulmus_MPN_Listesi.Add(RemoveIgnoredCharacters(mpn));
+                }
+
+                List<string> urun_bulunan_mpnler = new List<string>();
+                foreach (string mpn in Donusturulmus_MPN_Listesi)
+                {
+                    string tempMPN = mpn.ToString();
+                    List<string> b = MPN_Listesi.Where(x => x != null && x.Contains(tempMPN)).ToList();
+                    
+                    if (b.Count() > 0)
+                    {
+                        int ham_mpn_indexi = Donusturulmus_MPN_Listesi.FindIndex(x => x.Contains(tempMPN));
+
+                        urun_bulunan_mpnler.Add(Ham_MPN_Listesi[ham_mpn_indexi]);
+                    }
+                    else
+                    {
+                        SearchItemWithMPN_InsertIntoFoundMPNs(tempMPN, b, Donusturulmus_MPN_Listesi, urun_bulunan_mpnler, Ham_MPN_Listesi);
+                    }
+
+                    //Bulunan ilk MPN'i değil, eşleşme olan tüm MPN'ler getirilmeli
+                    var a = MPN_Listesi.Where(x => x != null && x.Contains(tempMPN)).ToList();
+
+
+                    if (a.Count > 0)
+                    {
+                        foreach (string item in a)
+                        {
+                            int ham_mpn_indexi = Donusturulmus_MPN_Listesi.FindIndex(x => x == mpn);
+                            int index_a = MPN_Listesi.FindIndex(x => x == item);
+                            string __mpn = mpn_ime[index_a].Key.ToString();
+                            List<CompleteItems_v2> mpn_bulunan_urunler = db.CompleteItems_v2.Where(x => x.MPN == __mpn).ToList();
+
+                            MPN _mpn = new MPN();
+                            _mpn.LineNo = ham_mpn_indexi + 1;
+                            _mpn.customerMPN = Ham_MPN_Listesi[ham_mpn_indexi];
+                            _mpn.items = mpn_bulunan_urunler.ToList();
+                            result.Add(_mpn);
+                        }
+                    }
+                    else
+                    {
+                        int ham_mpn_indexi = Donusturulmus_MPN_Listesi.FindIndex(x => x == mpn);
+                        MPN m = new MPN();
+                        m.LineNo = ham_mpn_indexi + 1;
+                        m.customerMPN = Ham_MPN_Listesi[ham_mpn_indexi];
+                        m.items = new List<CompleteItems_v2>();
+
+                        result.Add(m);
+                    }
+                }
+
+                List<_Item> items = new List<_Item>();
+                foreach (MPN item in result)
+                {
+                    if (item.items.Count > 0)
+                    {
+                        foreach (CompleteItems_v2 i in item.items)
+                        {
+                            _Item x = new _Item();
+                            x.LineNo = item.LineNo;
+                            x.customerMPN = item.customerMPN;
+                            x.item = i;
+
+                            items.Add(x);
+                        }
+                    }
+                    else
+                    {
+                        _Item x = new _Item();
+                        x.LineNo = item.LineNo;
+                        x.customerMPN = item.customerMPN;
+                        x.item = null;
+
+                        items.Add(x);
+                    }
+
+                }
+
+                foreach (_Item item in items)
+                {
+                    ItemDetailFill_Row(item);
+                }
+
+                string temp = String.Empty;
+                Color c = new Color();
+                foreach (DataGridViewRow row in dgItems.Rows)
+                {
+                    if (temp != row.Cells[dgMPN.Index].Value.ToString())
+                    {
+                        temp = row.Cells[dgMPN.Index].Value.ToString();
+                        c = Color.FromArgb((64 + rnd.Next(128)), (64 + rnd.Next(128)), (64 + rnd.Next(128)));
+                    }
+                    row.DefaultCellStyle.BackColor = c;
+                }
+            }
+            else
+            {
+                MessageBox.Show("File not selected!", "Excel");
+            }
+        }
+        
+        void SearchItemWithMPN_InsertIntoFoundMPNs(string tempMPN, List<string> list, List<string> convertedMpnList, List<string> ItemFoundMPNs, List<string> RawMPNList)
+        {
+            if (tempMPN.Length >= 8)
+            {
+                tempMPN = tempMPN.Substring(0, tempMPN.Length - 2);
+                list = MPN_Listesi.Where(x => x != null && x.Contains(tempMPN)).ToList();
+                if (list.Count() > 0)
+                {
+                    int ham_mpn_indexi = convertedMpnList.FindIndex(x => x.Contains(tempMPN));
+
+                    ItemFoundMPNs.Add(RawMPNList[ham_mpn_indexi]);
+                }
+                else
+                {
+                    SearchItemWithMPN_InsertIntoFoundMPNs(tempMPN, list, convertedMpnList, ItemFoundMPNs, RawMPNList);
+                }
+            }
+        }
+
+        private void GetAllItems()
+        {
+            mpn_ime = db.MPN_List.GroupBy(x => x.MPN).ToList();
+            foreach (var item in mpn_ime)
+            {
+                if (item.Key != null)
+                {
+                    MPN_Listesi.Add(RemoveIgnoredCharacters(item.Key));
+                }
+                else
+                {
+                    MPN_Listesi.Add(null);
+                }
+            }
+        }
+
+        private string RemoveIgnoredCharacters(string mpn)
+        {
+            string temp_mpn = mpn.Replace("-", "");
+            temp_mpn = temp_mpn.Replace("_", "");
+            temp_mpn = temp_mpn.Replace("/", "");
+            temp_mpn = temp_mpn.Replace("&", "");
+            temp_mpn = temp_mpn.Replace(" ", "");
+            temp_mpn = temp_mpn.Replace("\"", "");
+            temp_mpn = temp_mpn.Replace("#", "");
+            temp_mpn = temp_mpn.Replace("^", "");
+            temp_mpn = temp_mpn.Replace("(", "");
+            temp_mpn = temp_mpn.Replace(")", "");
+            temp_mpn = temp_mpn.Replace(",", "");
+            temp_mpn = temp_mpn.Replace("+", "");
+            temp_mpn = temp_mpn.Replace("*", "");
+            temp_mpn = temp_mpn.Replace("!", "");
+            temp_mpn = temp_mpn.Replace("%", "");
+            temp_mpn = temp_mpn.Replace("\\", "");
+            temp_mpn = temp_mpn.Replace("=", "");
+            temp_mpn = temp_mpn.Replace("?", "");
+            temp_mpn = temp_mpn.Replace(".", "");
+            temp_mpn = temp_mpn.Replace(",", "");
+            return temp_mpn;
+        }
+
+        private bool ChooseFile(List<string> Ham_MPN_Listesi)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "txt files (*.xlsx)|*.xlsx";
+            DialogResult result1 = dialog.ShowDialog();
+            if (result1 == DialogResult.OK)
+            {
+                Microsoft.Office.Interop.Excel.Application excel = new Microsoft.Office.Interop.Excel.Application();
+                Workbook wb = excel.Workbooks.Open(dialog.FileName);
+                Worksheet ws = wb.Worksheets[1];
+                int i = 2;
+                while (!String.IsNullOrEmpty(ws.Cells[i, 1].Value?.ToString()))
+                {
+                    string text = ws.Cells[i, 1].Value.ToString();
+                    Ham_MPN_Listesi.Add(text);
+                    i++;
+                }
+
+                wb.Close(0);
+                excel.Quit();
+                Marshal.ReleaseComObject(excel);
+
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        private void ItemDetailFill_Row(_Item rowItem)
+        {
+            DataGridViewRow row = dgItems.Rows[dgItems.Rows.Add()];
+
+            row.Cells[dgNo.Index].Value = rowItem.LineNo;
+            row.Cells[dgMPN.Index].Value = rowItem.customerMPN;
+            row.Cells[dgRsMPN.Index].Value = rowItem.item?.MPN;
+            row.Cells[dgArticleNo.Index].Value = rowItem.item?.Article_No;
+            int? qty = 0;
+            if(rowItem.item?.Pack_Quantity > rowItem.item?.Unit_Content)
+            {
+                qty = (int)rowItem.item?.Pack_Quantity;
+            }
+            else
+            {
+                qty = rowItem.item?.Unit_Content;
+            }
+            row.Cells[dgPackQuantity.Index].Value = qty;
+            if (rowItem.item != null)
+            {
+                row.Cells[dgUnitMeasure.Index].Value = (!String.IsNullOrEmpty(rowItem.item?.Unit_Measure)) ? rowItem.item.Unit_Measure : "EACH";
+            }
+            row.Cells[dgArticleDesc.Index].Value = rowItem.item?.Article_Desc;
+            row.Cells[dgManufacturer.Index].Value = rowItem.item?.Manufacturer;
+            row.Cells[dgPrice.Index].Value = (rowItem.item?.Col1Price * Convert.ToDecimal(7.2));
+            row.Cells[dgMHCodeLevel1.Index].Value = rowItem.item?.MH_Code_Level_1;
+        }
+    }
+
+    class MPN
+    {
+        public int LineNo { get; set; }
+        public string customerMPN { get; set; }
+        public List<CompleteItems_v2> items { get; set; }
+    }
+
+    class _Item
+    {
+        public int LineNo { get; set; }
+        public string customerMPN { get; set; }
+        public CompleteItems_v2 item { get; set; }
+    }
+}
